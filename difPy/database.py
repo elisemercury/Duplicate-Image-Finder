f7eb4be6257cf1cfa8ee3bac02267af46296b933
AliSot2000 <alisot200@gmail.com>
2023-01-08 03:04:49 +0100

    + Added more columns

--- difPy/database.py
+++ difPy/database.py
@@ -511,8 +511,10 @@ class Database:
                            "key INTEGER PRIMARY KEY AUTOINCREMENT , "
                            "key_a INTEGER NOT NULL , "
                            "key_b INTEGER NOT NULL ,"
-                           "dif REAL CHECK (dif_table.dif > 0),"
-                           "b_dir_b INTEGER CHECK (dif_table.b_dir_b >= 0 AND dif_table.b_dir_b <= 1) DEFAULT 0) ")
+                           "dif REAL CHECK (dif_table.dif > 0) DEFAULT 100,"
+                           "b_dir_b INTEGER CHECK (dif_table.b_dir_b >= 0 AND dif_table.b_dir_b <= 1) DEFAULT 0,"
+                           "error TEXT,"
+                           "success INT CHECK (dif_table.success >= 0 AND dif_table.success <= 1)) ")
 
     def test_dif_table_existence(self):
         """
