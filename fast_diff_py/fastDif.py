fb4c4b7b81821b7d6de28932c4951d4462cb635c
AliSot2000 <alisot200@gmail.com>
2023-06-30 11:09:09 +0200

    + Todo

--- fast_diff_py/fastDif.py
+++ fast_diff_py/fastDif.py
@@ -40,6 +40,7 @@ Features:
 # TODO single processing handler
 # TODO Implement process stop recovery.
 # TODO plot table is redundant. Use key from diff table and if create plot insert an empty row in the diff table.
+# TODO bulk processing first loop.
 # ----------------------------------------------------------------------------------------------------------------------
 # FEATURES
 # ----------------------------------------------------------------------------------------------------------------------
