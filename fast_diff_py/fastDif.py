b305472c2af2f008e4b080432d2c4f78dfa6ed52
AliSot2000 <alisot200@gmail.com>
2023-07-07 16:49:19 +0200

    + Importing the remove_existing_config function.

--- fast_diff_py/fastDif.py
+++ fast_diff_py/fastDif.py
@@ -12,7 +12,7 @@ import threading as th
 from fast_diff_py.datatransfer import *
 from concurrent.futures import ProcessPoolExecutor
 from fast_diff_py.sql_database import SQLiteDatabase
-from fast_diff_py.config import FastDiffPyConfig, test_existing_config
+from fast_diff_py.config import FastDiffPyConfig, test_existing_config, remove_existing_config
 from fast_diff_py.fast_diff_base import FastDiffPyBase
 from fast_diff_py.child_processes import parallel_resize, parallel_compare, find_best_image
 from fast_diff_py.child_processes import first_loop_dequeue_worker, first_loop_enqueue_worker
