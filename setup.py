fd5a6ebfc20e6966360464066f1f5b35ce7427a4
AliSot2000 <alisot200@gmail.com>
2023-07-09 01:35:01 +0200

    + New version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   # packages = ['fast_diff_py'],   # Chose the same as "name"
-  version = '0.2.0',      # Start with a small number and increase it with every change you make
+  version = '0.2.1',      # Start with a small number and increase it with every change you make
   url = 'https://github.com/AliSot2000/Fast-Image-Deduplicator',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/AliSot2000/Fast-Image-Deduplicator/archive/refs/tags/v0.2.0.tar.gz',    # I explain this later on
   keywords = ['python', 'image deduplicator', 'fast image deduplicator'],   # Keywords that define your package best
