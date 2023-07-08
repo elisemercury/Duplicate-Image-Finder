d3c35d4c85d0240d21ecef88b34ae7356086bcbe
AliSot2000 <alisot200@gmail.com>
2023-07-08 15:11:28 +0200

    + Trying to implement facilities to have a graceful shutdown.

--- src/fast_diff_py/child_processes.py
+++ src/fast_diff_py/child_processes.py
@@ -9,6 +9,35 @@ import queue
 import os
 from typing import Tuple, Union, List
 from types import FunctionType
+import signal
+
+
+class GracefulWorker:
+    interrupts: int = 0
+    interrupt_threshold: int = 2
+    identifier: int
+
+    def __init__(self, identifier: int):
+        """
+        Attach handlers for interrupts
+        :param identifier: identifier of this class
+        """
+        self.identifier = identifier
+        signal.signal(signal.SIGINT, self.handle_interrupt)
+        signal.signal(signal.SIGTERM, self.handle_interrupt)
+
+    def handle_interrupt(self):
+        """
+        Handle the interrupt if more than `interrupt_threshold` interrupts are received, exit immediately, otherwise
+        wait for the stop to come through the parent process.
+        :return:
+        """
+        print(f"{self.identifier} - Interrupt")
+        if self.interrupts >= self.interrupt_threshold:
+            print(f"{self.identifier:03} - Exiting immediately")
+            exit(1)
+
+        self.interrupts += 1
 
 def process_image(proc: CPUImageProcessing, args: PreprocessArguments) -> PreprocessResults:
     """
