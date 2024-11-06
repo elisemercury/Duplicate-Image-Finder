074966280fa281be3c6029cb282df14f22091c87
AliSot2000 <alisot200@gmail.com>
2024-11-06 14:54:45 +0100

    + Added an instance of a baseclass for Processes

---
+++ src/fast_diff_py/base_process.py
@@ -0,0 +1,32 @@
+import signal
+
+
+class GracefulWorker:
+    interrupts: int = 0
+    interrupt_threshold: int = 2
+    identifier: int
+
+    run: bool = True
+
+    def __init__(self, identifier: int):
+        """
+        Attach handlers for interrupts
+        :param identifier: identifier of this class
+        """
+        self.identifier = identifier
+        signal.signal(signal.SIGINT, self.handle_interrupt)
+        signal.signal(signal.SIGTERM, self.handle_interrupt)
+
+    def handle_interrupt(self, signal_type:int, frame: object):
+        """
+        Handle the interrupt if more than `interrupt_threshold` interrupts are received, exit immediately, otherwise
+        wait for the stop to come through the parent process.
+        :return:
+        """
+        print(f"{self.identifier:03} - Interrupt")
+        self.run = False
+        if self.interrupts >= self.interrupt_threshold:
+            print(f"{self.identifier:03} - Exiting immediately")
+            exit(1)
+
+        self.interrupts += 1
