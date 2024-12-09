8abe0fadcd4d452511e515757c400a84eeb76297
AliSot2000 <alisot200@gmail.com>
2024-12-09 14:28:50 +0100

    + Removed the testing of the original

--- scripts/testing_original.py
+++
@@ -1,5 +0,0 @@
-import src.fast_diff_py.dif
-
-
-if __name__ == "__main__":
-    c = src.fast_diff_py.dif(directory_A="/home/alisot2000/Desktop/SAMPLE_MIRA/JOIN/", show_output=True, show_progress=True)
