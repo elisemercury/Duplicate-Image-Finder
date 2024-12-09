c757d589d7f87dc1754343f53f225194605bec54
AliSot2000 <alisot200@gmail.com>
2024-12-09 13:56:31 +0100

    + Updated Partition mode + Implemented Duplicate Mode

--- scripts/duplicate_generator.py
+++ scripts/duplicate_generator.py
@@ -28,7 +28,14 @@ def remove_prefix(text, prefix):
         return text[len(prefix):]
     return text
 
-def partition(source: str, dir_a: str, dir_b: str,pd: float = 0.001, pb: float = 0.5, sym: bool = False, limit: int = 40000) -> Tuple[int, int, int]:
+
+def partition(source: str,
+              dir_a: str,
+              dir_b: str,
+              pd: float = 0.001,
+              pb: float = 0.5,
+              op: str = "MOVE",
+              limit: int = 40000) -> Tuple[int, int, int]:
     """
     Partition mode to generate duplicates. Uses Symlinks in dir_a and dir_b to link to the files in the src directory
 
@@ -37,12 +44,26 @@ def partition(source: str, dir_a: str, dir_b: str,pd: float = 0.001, pb: float =
     :param dir_b: The second directory
     :param pd: The probability of duplication
     :param pb: The probability of moving to dir_b
+    :param op: The operation to perform (MOVE, COPY, LINK)
+    :param limit: The limit of files to process
+
     :return: Number of files in dir a, number of files in dir b, number of duplicates
     """
+    op = op.upper()
+    if op not in ["MOVE", "COPY", "LINK"]:
+        raise ValueError(f"Operation {op} not supported")
 
-    def partition_internal(src: str, cur: str, dir_a: str, dir_b: str, pd: float, pb: float, sym: bool, limit: int) \
-            -> Tuple[int, int, int]:
-        a, b, d = 0, 0, 0
+    def partition_internal(src: str,
+                           cur: str,
+                           dir_a: str,
+                           dir_b: str,
+                           pd: float,
+                           pb: float,
+                           limit: int,
+                           op: str,
+                           ca: int, cb: int, cd: int) -> Tuple[int, int, int]:
+
+        a, b, d = ca, cb, cd
         abs_src = os.path.abspath(src)
         abs_a = os.path.abspath(dir_a)
         abs_b = os.path.abspath(dir_b)
@@ -51,15 +72,12 @@ def partition(source: str, dir_a: str, dir_b: str,pd: float = 0.001, pb: float =
         cb = os.path.join(abs_b, cur)
 
         for f in os.listdir(cp):
-            if a > limit and b > limit:
+            if limit is not None and (a > limit or b > limit):
                 return a, b, d
 
             # If it's a directory, we need to recurse
             if os.path.isdir(os.path.join(cp, f)):
-                na, nb, nd = partition_internal(src, os.path.join(cur, f), dir_a, dir_b, pd, pb, sym, limit)
-                a += na
-                b += nb
-                d += nd
+                a, b, c = partition_internal(src, os.path.join(cur, f), dir_a, dir_b, pd, pb, limit, op, a, b, d)
 
             # If it's a file, we need to copy it
             elif os.path.isfile(os.path.join(cp, f)):
@@ -73,12 +91,15 @@ def partition(source: str, dir_a: str, dir_b: str,pd: float = 0.001, pb: float =
                     if not os.path.exists(cb):
                         os.makedirs(cb)
 
-                    if sym:
+                    if op == "LINK":
                         os.symlink(os.path.join(cp, f), os.path.join(ca, f))
                         os.symlink(os.path.join(cp, f), os.path.join(cb, f))
-                    else:
+                    elif op == "MOVE":
                         shutil.move(os.path.join(cp, f), os.path.join(ca, f))
                         shutil.copy(os.path.join(ca, f), os.path.join(cb, f))
+                    elif op == "COPY":
+                        shutil.copy(os.path.join(cp, f), os.path.join(ca, f))
+                        shutil.copy(os.path.join(cp, f), os.path.join(cb, f))
                     a, b, d = a + 1, b + 1, d + 1
 
                 # Symlink to either or
@@ -87,43 +108,86 @@ def partition(source: str, dir_a: str, dir_b: str,pd: float = 0.001, pb: float =
                     if random.random() < pb:
                         if not os.path.exists(cb):
                             os.makedirs(cb)
-                        if sym:
+                        if op == "LINK":
                             os.symlink(os.path.join(cp, f), os.path.join(cb, f))
-                        else:
+                        elif op == "MOVE":
                             shutil.move(os.path.join(cp, f), os.path.join(cb, f))
+                        elif op == "COPY":
+                            shutil.copy(os.path.join(cp, f), os.path.join(cb, f))
                         b += 1
 
                     # Symlink to dir_a
                     else:
                         if not os.path.exists(ca):
                             os.makedirs(ca)
-                        if sym:
+                        if op == "LINK":
                             os.symlink(os.path.join(cp, f), os.path.join(ca, f))
-                        else:
+                        elif op == "MOVE":
                             shutil.move(os.path.join(cp, f), os.path.join(ca, f))
+                        elif op == "COPY":
+                            shutil.copy(os.path.join(cp, f), os.path.join(ca, f))
                         a += 1
             else:
                 print(f"Skipping {f}")
 
         return a, b, d
 
-    return partition_internal(source, "", dir_a, dir_b, pd, pb, sym=sym, limit=limit)
+    return partition_internal(source, "", dir_a, dir_b, pd, pb, op=op, limit=limit, ca=0, cb=0, cd=0)
 
 
-def duplicate(src: str, dst: str, pc: float = 0.5) -> Tuple[int, int]:
+def duplicate(src: str, dst: str, pc: float = 0.5, op: str = "COPY", limit: int = None) -> Tuple[int, int]:
     """
     Duplicate mode to generate duplicates.
 
     :param src: The first directory
     :param dst: The second directory
-    :param pc: The probability of copying
+    :param pc: The probability of copying. Copy iff random.random() < pc
+    :param op: The operation to perform (COPY, LINK)
+    :param limit: The limit of files to process (number of duplicates
+
     :return: Number of files in dir a, number of files in dir b
     """
-    ...
-    return 0, 0
+
+    op = op.upper()
+    if op not in ["COPY", "LINK"]:
+        raise ValueError(f"Operation {op} not supported")
+
+    def duplicate_internal(src: str, dst: str, cur: str, pc: float, op: str, limit: int, s: int, c: int):
+        abs_src = os.path.abspath(src)
+        abs_dst = os.path.abspath(dst)
+        cp = os.path.join(abs_src, cur)
+        cd = os.path.join(abs_dst, cur)
+
+        for f in os.listdir(cp):
+            if limit is not None and c > limit:
+                return s, c
+
+            # If it's a directory, we need to recurse
+            if os.path.isdir(os.path.join(cp, f)):
+                s, c = duplicate_internal(src, dst, os.path.join(cur, f), pc, op, limit, s, c)
+
+            # If it's a file, we need to copy it
+            elif os.path.isfile(os.path.join(cp, f)):
+                s += 1
+                if random.random() < pc:
+                    # Create directory in dst
+                    if not os.path.exists(cd):
+                        os.makedirs(cd)
+
+                    if op == "LINK":
+                        os.symlink(os.path.join(cp, f), os.path.join(cd, f))
+                    elif op == "COPY":
+                        shutil.copy(os.path.join(cp, f), os.path.join(cd, f))
+
+                    c += 1
+            # We've got something we don't know.
+            else:
+                print(f"Skipping {f}")
+
+    return duplicate_internal(src, dst, "", pc, op, limit, 0, 0)
 
 
 if __name__ == "__main__":
     print(partition(source="/media/alisot2000/MacBeth/dedup_benchmarks/IMDB-Bench/",
-                    dir_a="/media/alisot2000/MacBeth/workbench_large/dir_a",
-                    dir_b="/media/alisot2000/MacBeth/workbench_large//dir_b", pd=0.01, pb=0.5))
+                    dir_a="/media/alisot2000/MacBeth/workbench_tiny/dir_a",
+                    dir_b="/media/alisot2000/MacBeth/workbench_tiny/dir_b", pd=0.01, pb=0.5, limit=5000, op="COPY"))
