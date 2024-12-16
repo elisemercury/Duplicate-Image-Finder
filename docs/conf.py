cf99972fce2aa53b30d92880033826d814e76cc6
elisemercury <elisejlandman@hotmail.com>
2024-12-16 17:43:37 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,11 +36,6 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
-html_logo = "static/logos/logo-min.png"
-html_theme_options = {
-    'logo_only': True,
-    'display_version': True,
-}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
