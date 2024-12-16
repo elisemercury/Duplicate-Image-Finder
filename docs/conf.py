d2fa3fc850f16641eff9f28bb07166e22161fe14
elisemercury <elisejlandman@hotmail.com>
2024-12-16 17:35:51 +0000

    Upload smaller logo

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
-html_logo = "static/logos/logo.png"
+html_logo = "static/logos/logo-min.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': True,
